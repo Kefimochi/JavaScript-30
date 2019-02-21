a60a25f92f75207b1f01931fc5f74f52c6d49da2
Kate Efimova <kate.efimova0@gmail.com>
2019-02-20 21:43:05 -0800

    Update DrumKitJS.js

--- Drum Kit/DrumKitJS.js
+++ Drum Kit/DrumKitJS.js
@@ -1,18 +1,39 @@
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+      this.classList.remove('playing');
   }
-  function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
+
+  function playSound(isClick, e) {
+    if (isClick === true) {
+      let target = e.currentTarget;
+      target.classList.add('playing');
+
+      for (let i = 0; i < keys.length; i++) {
+         if (target.dataset.key === keys[i].dataset.key) {
+           const audio = document.querySelector(`audio[data-key="${keys[i].dataset.key}"]`);
+           audio.currentTime = 0;
+           audio.play();
+       }
+      }
+    }
     
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
+    if (isClick === false) {
+      const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+      const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+      
+       if (!audio) return;
+       key.classList.add('playing');
+       audio.currentTime = 0;
+       audio.play();
+    }
   }
   const keys = document.querySelectorAll('.key');
-    //Since keys is an array, we can't just call addEventListener
+    //Since keys is an array, can't just call addEventListener
     //Instead, for each key add an event listener
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+  keys.forEach(key => key.addEventListener('mousedown',function(e) {
+    playSound(true, e);
+}));
+  window.addEventListener('keydown', function(e) {
+    playSound(false, e);
+});
