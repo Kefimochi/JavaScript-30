abf992d764cf4e1a592484abd75a0b128c4c220f
Kate Efimova <kate.efimova0@gmail.com>
2019-03-02 15:50:55 -0800

    Update flexPanels.js

--- Flex Panels Image Gallery/flexPanels.js
+++ Flex Panels Image Gallery/flexPanels.js
@@ -3,18 +3,14 @@ panels.forEach(panel => panel.addEventListener('mouseover', hovered));
 panels.forEach(panel => panel.addEventListener('mouseout', notHovered))
 
 function hovered() {
-  setTimeout(() => {
     this.classList.toggle('open');
-  }, 300);
   setTimeout(() => {
     this.classList.toggle('open-active');
   }, 700);
 }
 
 function notHovered() {
-  setTimeout(() => {
     this.classList.remove('open');
-  }, 300);
   setTimeout(() => {
     this.classList.toggle('open-active');
   }, 700);
