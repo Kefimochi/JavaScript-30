8d5792b6488221de79dfc3cb177a22b87ff46848
Kate Efimova <kate.efimova0@gmail.com>
2019-03-12 22:49:18 -0700

    Create shift.js

---
+++ Hold Shift/shift.js
@@ -0,0 +1,57 @@
+//Select All for box color change
+const select = document.querySelectorAll('.selectJS');
+select.forEach(box => {
+  box.addEventListener('mousedown', selectAll)
+  box.addEventListener('mouseup', remove);
+});
+
+//Clear All for box color change
+const clear = document.querySelectorAll('.clearJS');
+clear.forEach(box => {
+  box.addEventListener('mousedown', clearAll);
+  box.addEventListener('mouseup', remove);
+});
+
+//Listening to all input boxes
+const boxes = document.querySelectorAll('input');
+boxes.forEach((input, index) => input.addEventListener('mousedown', function(e) {
+    isShift(e, index);
+}));
+let boxesArr = [...boxes];
+firstBox = 0;
+
+//Selects all checkboxes
+function selectAll(e) {
+  this.classList.add('selectAll');
+  var aa = document.querySelectorAll("input[type=checkbox]");
+    for (var i = 0; i < aa.length; i++){
+        aa[i].checked = true;
+    }
+}
+
+//Clear all checkboxes
+function clearAll(e) {
+  this.classList.add('clearAll');
+  var aa = document.querySelectorAll("input[type=checkbox]");
+    for (var i = 0; i < aa.length; i++){
+        aa[i].checked = false;
+    }
+}
+
+//Removes gray color after mouseup on select and clear
+function remove(e) {
+  this.classList.remove('clearAll');
+  this.classList.remove('selectAll');
+}
+
+//If shift is pressed then check multiple checkboxes
+function isShift(e, index) {
+   if(firstBox && e.shiftKey) {
+    for (let i = firstBox; i < index; i++){
+        boxesArr[i].checked = true;
+    } 
+  } else {
+    firstBox = index;
+    console.log("first "+ firstBox);
+  }
+}
